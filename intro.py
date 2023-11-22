517b5f8c7ce3046b13fd7788bf467551bdc3deb1
Natthyx <natannanz16@gmail.com>
2023-11-22 15:37:06 +0300

    second class

--- intro.py
+++ intro.py
@@ -1,1 +1,60 @@
-print("Hello World")
+# print("Hello World")
+# x = "string"
+# y = "True"
+# print(type(x))
+# print(x+y)
+# x = 4.0
+# y = 5
+# print(x+y)
+
+# x= 31
+# x+=x
+# print(x)
+
+# x = "blackwolf"
+# print(x[2:])
+# print(x[-1])
+
+# x = "Google Development Student Club"
+# print(x[-3:-1])
+
+# a= [1,2,3]
+# print(a[2])
+# b=a*10
+# print(b)
+# a = 5
+# print(a//2)
+
+# a= [1]
+# b =[a,a]
+# a[0]=3
+
+# print(a,b)
+
+# a = [1]
+# b =[2,a] #[2,[1]]
+# c =[3,a,b] #[3,[1],[2,[1]]]
+# print(b,c)
+
+# a = 3  
+# b = 6
+# c = b/a  # 2.0
+# a = b%c # 0.0
+# b = a+b # 6.0
+# c = b+c #8.0
+
+# print(a,b,c)
+
+# a,c= 2,3
+# c,b = a,c+1
+# print(a,b,c)
+
+# x,y = 2,6
+# x,y = y,x+2
+# print (x,y)
+
+x = input("Enter your full name: ")
+print(x)
+print(x[0])
+print(x[:5])
+print(x[3:6])
